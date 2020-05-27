cca2dbf142d85f60fe4500ff8a9221453f191257
rick_tan <rick_tan@qq.com>
2020-05-27 22:59:23 +0800

    update

--- LibOkhttp/src/androidTest/java/com/squareup/okhttp3/ExampleInstrumentedTest.java
+++ LibOkhttp/src/androidTest/java/com/squareup/okhttp3/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
-package com.android.volley;
+package com.squareup.okhttp3;
 
 import android.content.Context;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.android.volley.test", appContext.getPackageName());
+        assertEquals("com.squareup.okhttp3.test", appContext.getPackageName());
     }
 }
--- LibOkhttp/src/test/java/com/squareup/okhttp3/ExampleUnitTest.java
+++ LibOkhttp/src/test/java/com/squareup/okhttp3/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.android.volley;
+package com.squareup.okhttp3;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
